33238c2d2fe9be522c8e11636d569ad33d766974
Geoff Lee <g.lee@ed.ac.uk>
2018-02-04 16:03:23 +0000

    Hide unzipping in the Provider; make it pass pylint

--- git2jss/PythonBDistBuilder.py
+++ git2jss/PythonBDistBuilder.py
@@ -11,18 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Processor that creates a file"""
-
+"""Processor to build a python distutils project"""
 import os
+from zipfile import ZipFile
 from subprocess import check_call
+# Pylint can't load autopkglib, so stop it moaning
+#pylint: disable=locally-disabled,import-error
 from autopkglib import Processor, ProcessorError
 
 
 __all__ = ["PythonBDistBuilder"]
 
-
+#pylint: disable=locally-disabled,too-few-public-methods
 class PythonBDistBuilder(Processor):
-    """Build a python disttools package, ready for packaging"""
+    """Build a python disttools project, ready for packaging"""
     description = __doc__
     input_variables = {
         "source_path": {
@@ -33,16 +35,30 @@ class PythonBDistBuilder(Processor):
     output_variables = {
     }
 
+#pylint: disable=
+
     def main(self):
+        """Do it"""
         try:
             os.chdir(self.env['source_path'])
-            check_call(['/usr/bin/python', 'setup.py', 'bdist', '-p', 'macOS', '--formats', 'zip'])
+            check_call(['/usr/bin/python', 'setup.py',
+                        'bdist', '-p', 'macOS', '--formats', 'zip'])
             self.output("Built dist at %s" % self.env['source_path'])
         except BaseException, err:
             raise ProcessorError("Can't build dist at %s: %s"
                                  % (self.env['source_path'], err))
 
+        cwd = os.getcwd()
+        pkgroot = cwd + '../pkg_root'
+        try:
+            zipped = ZipFile('./dist/' + self.env['NAME'] +
+                             '-' + self.env['RELEASE'] + '.macOS.zip')
+            zipped.extractall(path=pkgroot)
+            self.output("Unzipped package root at %s" % self.env['source_path'])
+        except BaseException, err:
+            raise ProcessorError("Can't extract package root at %s: %s"
+                                 % (pkgroot, err))
+
 if __name__ == '__main__':
     PROCESSOR = PythonBDistBuilder()
     PROCESSOR.execute_shell()
-
